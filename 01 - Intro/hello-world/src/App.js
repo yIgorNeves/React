89835bdf7ffcca887e051725307f5fe989903aee
Igor Neves <32842877+yIgorNeves@users.noreply.github.com>
2022-04-26 13:27:02 -0300

    Primeiro exercicio em React - 26/04/2022

--- 01 - Intro/hello-world/src/App.js
+++ 01 - Intro/hello-world/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        h1 > Hello World React < /h1> <
-        /div>
+    return ( 
+        <div className = "App">
+        <h1> Hello World React </h1> 
+        <h2> Emmet Funciona!!!</h2>  
+        <p>Feito por Igor Neves. 26/04/2022</p>   
+        <h4>Muito obrigado!</h4>    
+        </div>
     );
 }
 
