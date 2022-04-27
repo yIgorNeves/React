29094fe690a28bb5aaeca3b127df8850a3367cd4
Igor Neves <32842877+yIgorNeves@users.noreply.github.com>
2022-04-27 20:10:47 -0300

    Edição dos readme.md

--- 03 - Avançando no React/avancando/src/App.js
+++ 03 - Avançando no React/avancando/src/App.js
@@ -1,9 +1,20 @@
+//imagens
+import City from "./assets/city.jpg"
+//styles | CSS
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div>Avançando em React</div>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Imagem em asset*/}
+      <div>
+        <img src={City} alt="Cidade" />
+      </div>
     </div>
   );
 }
